6722659b63c7abafe07b50530b7c4088fe5ff34a
Nikolaj <43247719+nikolaj-obh@users.noreply.github.com>
2022-08-09 15:19:53 +0200

    Update game.h

--- utils/game.h
+++ utils/game.h
@@ -1,8 +1,8 @@
-#ifndef MANDATORY2_GAME_H
-#define MANDATORY2_GAME_H
+#ifndef C-PROGRAMMING-EXAM_GAME_H
+#define C-PROGRAMMING-EXAM_GAME_H
 
 typedef struct game {
     int *numbers;
 } Game; // Save the numbers that appear during the game
 
-#endif //MANDATORY2_GAME_H
+#endif //C-PROGRAMMING-EXAM_GAME_H
